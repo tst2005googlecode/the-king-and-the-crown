a1083da7823f6c264a9d882a3fed8b1bb096c107
damien.carol <damien.carol@02f040b4-a015-a16b-2f7b-ab45a99db5db>
2010-05-03 14:33:27 +0000

    Improved rendering of unit animation.

--- trunk/src/unit.lua
+++ trunk/src/unit.lua
@@ -30,21 +30,62 @@ function Unit.create()
     temp.unittype = UnitType.UNKNOW
     temp.selected = 0
     temp.velocity = 100
+    
+    temp.direction = "down"
+    temp.moving = false
+    
+    -- data for animation
+    temp.frame_num_time = 0
+    temp.frame_num = 1
+
     return temp
 end
 
 function Unit:update(dt, up, down, left, right)
-    if up == 1 then
+
+    -- update coordinates
+    if up == true then
         self.y = self.y - dt * self.velocity
-    end
-    if down == 1 then
+    elseif down == true then
         self.y = self.y + dt * self.velocity
     end
-    if left == 1 then
+    if left == true then
         self.x = self.x - dt * self.velocity
-    end
-    if right == 1 then
+    elseif right == true then
         self.x = self.x + dt * self.velocity
+    end
+    
+    -- update direction for animation drawing
+    if left == true then
+        self.direction = "left"
+    elseif right == true then
+        self.direction = "right"
+    elseif up == true then
+        self.direction = "up"
+    elseif down == true then
+        self.direction = "down"
+    end
+    
+    if up == true or down == true or 
+        left == true or right == true then
+        self.moving = true
+    else
+        self.moving = false
+    end
+    
+    if self.moving == true then
+    -- updating the animation
+    self.frame_num_time = self.frame_num_time + dt
+    
+    if self.frame_num_time > 0.2 then
+        self.frame_num_time = self.frame_num_time - 0.2
+        self.frame_num = self.frame_num + 1
+    end
+
+    if self.frame_num > 4 then
+        self.frame_num = 1
+    end
+    
     end
 end
 
@@ -54,6 +95,20 @@ end
 
 function Unit:draw()
     
-    self.unittype:draw(self.x, self.y)
+    if self.moving then
+        if self.direction == "left" then
+            self.unittype:draw_backside(self.x, self.y, self.frame_num)
+        elseif self.direction == "right" then
+            self.unittype:draw_side(self.x, self.y, self.frame_num)
+        elseif self.direction == "up" then
+            self.unittype:draw_back(self.x, self.y, self.frame_num)
+        elseif self.direction == "down" then
+            self.unittype:draw_front(self.x, self.y, self.frame_num)
+        end
+    else
+        if self.direction == "left" then
+            self.unittype:draw_backside_stand(self.x, self.y, self.frame)
+        end
+    end
 
 end
